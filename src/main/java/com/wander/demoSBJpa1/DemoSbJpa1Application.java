6ae572742dd65e1682b916959c81b47a17e2cfce
wanderlorenz <wanderlorenz@outlook.com>
2020-04-29 00:59:00 -0300

    Project created

---
+++ src/main/java/com/wander/demoSBJpa1/DemoSbJpa1Application.java
@@ -0,0 +1,13 @@
+package com.wander.demoSBJpa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSbJpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wander/demoSBJpa1/DemoSbJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wander.demoSBJpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSbJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
